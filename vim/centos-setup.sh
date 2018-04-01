74e437eb8b2f0441c5d00835ab671e5a8ce89357
yiran <yiran@smartx.com>
2018-04-02 07:19:38 +0800

    Update vimrc and add tmux.conf

--- vim/centos-setup.sh
+++ vim/centos-setup.sh
@@ -8,7 +8,7 @@ fi
 
 if [ ! -d ~/.vim/bundle/Vundle.vim ];then
     echo "Install vundle ..."
-    git clone https://github.com/VundleVim/Vundle.vim.git  ~/.vim/bundle/Vundle
+    git clone https://github.com/VundleVim/Vundle.vim.git  ~/.vim/bundle/Vundle.vim
 fi
 
 cp -v vimrc ~/.vimrc
